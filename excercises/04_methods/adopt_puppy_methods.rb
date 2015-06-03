feefc42c6d01b4fc5a6f6a545f1fb56ebb434cc1
Justin Beall <jus.beall@gmail.com>
2015-06-03 14:40:47 -0400

    Update adopt_puppy_methods.rb

--- excercises/04_methods/adopt_puppy_methods.rb
+++ excercises/04_methods/adopt_puppy_methods.rb
@@ -39,6 +39,6 @@ adopt_puppy 1
 continue_shopping
 adopt_puppy 2
 proceed_to_checkout
-adoption_completion_form "Justin Beall", "4070 Stonehaven Rd.\nSouth Euclid, OH 44121", "jus.beall@gmail.com", "Credit card"
+adoption_completion_form "Justin Beall", "1234 Fake Rd.\City, OH 44112", "jus.beall@gmail.com", "Credit card"
 success_message_displays
 close_browser
